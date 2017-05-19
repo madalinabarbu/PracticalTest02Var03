5091f47cb4a00d7bc3d367c53903ef9952d1c96f
Student User <student@cti.pub.ro>
2017-05-19 10:32:34 +0300

    Created project

---
+++ PracticalTest02Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02var03/practicaltest02var03/PracticalTest02Var03MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest02var03.practicaltest02var03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest02Var03MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_var03_main);
+    }
+}
